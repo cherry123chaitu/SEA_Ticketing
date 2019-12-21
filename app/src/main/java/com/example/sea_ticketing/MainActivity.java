e0eb01fe0b1607a1b6058d9f4cc0e21581ec184d
cherry123chaitu <gbunnu1991@gmail.com>
2019-12-21 23:28:23 +0530

    Some More updates

--- app/src/main/java/com/example/sea_ticketing/MainActivity.java
+++ app/src/main/java/com/example/sea_ticketing/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.sea_ticketing;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+TextView contact;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        contact = findViewById(R.id.AdminLink);
+        contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String number= "9398161852";
+                Intent i= new Intent();
+                i.setAction(Intent.ACTION_DIAL);
+                i.setData(Uri.parse("tel:+"+number));
+                startActivity(i);
+            }
+        });
     }
+
 }
