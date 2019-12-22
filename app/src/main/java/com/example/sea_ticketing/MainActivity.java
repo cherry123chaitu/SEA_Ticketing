e588f93f8ad078c1f53be8872f70997533724a3e
cherry123chaitu <gbunnu1991@gmail.com>
2019-12-22 18:33:49 +0530

    Slight changes according to Nihant and geetika.
    Signed-off-by: cherry123chaitu <gbunnu1991@gmail.com>

--- app/src/main/java/com/example/sea_ticketing/MainActivity.java
+++ app/src/main/java/com/example/sea_ticketing/MainActivity.java
@@ -7,15 +7,18 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 TextView contact;
+EditText id,password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        id=findViewById(R.id.StudentId);
         contact = findViewById(R.id.AdminLink);
         contact.setOnClickListener(new View.OnClickListener() {
             @Override
