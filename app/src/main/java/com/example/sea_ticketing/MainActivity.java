8a6bd305c596e1304f5519ee4f272eba9e834d66
cherry123chaitu <gbunnu1991@gmail.com>
2019-12-22 20:12:57 +0530

    Login To Dashboard
    Signed-off-by: cherry123chaitu <gbunnu1991@gmail.com>

--- app/src/main/java/com/example/sea_ticketing/MainActivity.java
+++ app/src/main/java/com/example/sea_ticketing/MainActivity.java
@@ -7,19 +7,33 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-TextView contact;
-EditText id,password;
+private TextView contact;
+
+private EditText id,password;
+public  Button Login_Button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         id=findViewById(R.id.StudentId);
         contact = findViewById(R.id.AdminLink);
+        Login_Button =findViewById(R.id.LOGIN_BUTTON);
+        password=findViewById(R.id.password);
+
+        Login_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                click(view);
+            }
+        });
         contact.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -30,6 +44,20 @@ EditText id,password;
                 startActivity(i);
             }
         });
+    }
+    public void click(View v){
+        String uid,pass;
+        uid = id.getText().toString();
+        pass= password.getText().toString();
+        if(uid.equals("170030")&& (pass.equals("Chaitanya"))){
+            Toast.makeText(getApplicationContext(),"Welcome 170030",Toast.LENGTH_SHORT).show();
+        Intent i= new Intent(MainActivity.this,Dashboard.class);
+        startActivity(i);
+        }
+        else{
+            Toast.makeText(getApplicationContext(),"Wrong id or password please verify",Toast.LENGTH_SHORT).show();
+
+        }
     }
 
 }
