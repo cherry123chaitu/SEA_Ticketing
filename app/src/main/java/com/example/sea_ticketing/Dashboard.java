8a60f526dc9bfd5d6d2b06c2b72d8e7eef2699c8
cherry123chaitu <gbunnu1991@gmail.com>
2019-12-24 01:24:02 +0530

    Major update and Dashboard
    Signed-off-by: cherry123chaitu <gbunnu1991@gmail.com>

--- app/src/main/java/com/example/sea_ticketing/Dashboard.java
+++ app/src/main/java/com/example/sea_ticketing/Dashboard.java
@@ -9,6 +9,8 @@ public class Dashboard extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.AppThemeDark);
+        setTitle("Dashboard");
         setContentView(R.layout.activity_dashboard);
     }
 }
