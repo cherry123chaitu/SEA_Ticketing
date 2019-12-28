249e2ec585cf7fda8e8d7ec03a137752fade4011
cherry123chaitu <gbunnu1991@gmail.com>
2019-12-28 19:03:31 +0530

    Profile and updates on dashboard and Login screen
    Signed-off-by: cherry123chaitu <gbunnu1991@gmail.com>

--- app/src/main/java/com/example/sea_ticketing/Dashboard.java
+++ app/src/main/java/com/example/sea_ticketing/Dashboard.java
@@ -1,16 +1,29 @@
 package com.example.sea_ticketing;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Dashboard extends AppCompatActivity {
-
+    TextView profiletext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(R.style.AppThemeDark);
         setTitle("Dashboard");
+        profiletext=findViewById(R.id.profile_text);
         setContentView(R.layout.activity_dashboard);
+        }
+
+        public void Click(View view){
+        Intent i =new Intent(Dashboard.this,Profile.class);
+        startActivity(i);
+        }
     }
-}
+
+
